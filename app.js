d220d494250d493fcc2588106b919ff1b1503ff8
AnkushT27 <ankushtiwari102@gmail.com>
2020-08-25 12:21:52 +0530

    commit for setup users

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var db = require('./utils/db')
+var db = require('./utils/db');
+var authUserRoute = require('./routes/auth')
+let User = require('./models/users')
 var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
-app.post('/',(req,res)=>{
-    console.log(req.body)
-    res.send({message:req.body.message})
-    
-})
+app.use(authUserRoute)
+
 
 db.MongoDb((response)=>{
 console.log(response)
--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
-let dbs
-
 exports.MongoDb = async function(cb) {
-    try{
+try{
 let MongoConnection =await mongoose.connect('mongodb+srv://mongo:mongo@123@cluster0.6sdhd.mongodb.net/quiz?retryWrites=true&w=majority',{useNewUrlParser:true})
 cb(MongoConnection)
 }
