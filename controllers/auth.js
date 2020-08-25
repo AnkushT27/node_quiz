3ff7a445e3d372e26668f1e11a8e8e6516040d02
AnkushT27 <ankushtiwari102@gmail.com>
2020-08-25 21:03:39 +0530

    commit for login functionality plus error handling

---
+++ controllers/auth.js
@@ -0,0 +1,25 @@
+const User = require('../models/users');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+exports.login = async (req,res,next)=>{
+  let user = await User.findOne({email:req.body.email})
+  if(!user){
+      res.staus(404).send({"message":"User Not Found"})
+  }
+  else{
+      let isValidPassword = await bcrypt.compare(req.body.password,user.password)
+      if(isValidPassword){
+        const token = jwt.sign({
+            email : user.email
+        },'quiztoken',{expiresIn:'1h'})
+        res.status(200).json({
+            token:token
+        })
+      }
+      else{
+          const err = new Error('Password does not match')
+          err.statusCode = 401
+          next(err)
+      }
+  }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const {addUser} = require('../controllers/user');
+const {login} = require('../controllers/auth');
 const router = express.Router()
 
-router.post('/users',addUser)
+router.post('/users',addUser);
+router.post('/login',login)
 
 module.exports = router;
